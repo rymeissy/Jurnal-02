01f4291946d08822fccfe0ae8f30da9959c88f1b
rymeissy <ilikecat0101@gmail.com>
2021-09-27 17:42:25 +0700

    HelloWorld.java

---
+++ Pratikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		System.out.println ("Nama  : Meissy");
+		System.out.println ("NIM   : 6706213083");
+		System.out.println ("Kelas : 4503");
+	}
+}
